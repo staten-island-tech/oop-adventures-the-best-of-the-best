31e7f7ce4edd343b87d86053fc6ede640f0136c7
alinac1888 <alinac44@nycstudents.net>
2026-01-06 06:26:12 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 class npc:
-    def _init_(self, name, dialogue,)
-        se
+    def _init_(self, name, dialogue, quest, inventory):
+        self.name = name
+        self.dialogue = dialogue
+        dialogue = []
+        self.quest = quest
+        quest = []
+        self.inventory = inventory
+        inventory = []
+
+    def activity(self):
+        print("nananaan walking my radish")
+        if self.approach == "y"
+
+        
+
+Humble_Potato = npc("Humble Potato", ["No thank you, its ok, fan.", "nananaan im so humble"], ["Hello young lad! I "])        
+
+# "Thank you, with such gratitude and grace, for accepting request young, kind lad."
