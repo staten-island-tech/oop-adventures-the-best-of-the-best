3cecaf5af84a0a53f8f0d86bd592e85b619fcd67
kexinl124 <KExinl10@nycstudents.net>
2026-01-06 08:46:25 -0500

    Update app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+class enemy:
+    def __init__(self, name, inventory, stats, dialogue):
+        self.name = name
+        self.inventory = inventory
+        self.stats = stats
+        self.dialogue = dialogue
+
+        
