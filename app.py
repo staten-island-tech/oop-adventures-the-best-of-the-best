7029bfc1e670174e825a6d9a22f2effbbaad586c
kexinl124 <KExinl10@nycstudents.net>
2026-01-06 09:26:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-class enemy:
+class villian:
     def __init__(self, name, inventory, stats, dialogue):
         self.name = name
         self.inventory = inventory
         self.stats = stats
         self.dialogue = dialogue
+    
+    def steal(self,item):
+        self.inventory.append(item)
+        print(King_of_Yam.__dict__)
+
+King_of_Yam = villian("King of Yam", ["Prisoners of Yam"], ["Hp" == 500, "Hunger" == 100, "Atk" == 500], ["I'm am the all mighty King of Yam"])
+print(King_of_Yam.__dict__)
+
 
         
