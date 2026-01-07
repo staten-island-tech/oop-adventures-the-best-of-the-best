239f1a49f9fbc5a1f0293abc1e469861e797b35a
cadee-exe <cadeez@nycstudents.net>
2026-01-07 09:25:59 -0500

    Update mc.py

--- mc.py
+++ mc.py
@@ -1,5 +1,4 @@
 class mc:
-
     def __init__(self, name, inventory, stats, dialogue, quest):
         self.name = name
         self.inventory = inventory
@@ -7,11 +6,14 @@ class mc:
         self.dialogue = dialogue
         self.quest = quest
 
-
-    def approach(self):
-        speak = input("Do you want to approach? y/n")
-        if "y":
-            
+    
 
 
-     
+Wavely_Potato = mc("Wavely_Potato", ["sword, chips, health orb"], "Health = 100, Hunger = 100, Atk = 15", ["I am Wavely Potato. i embark on a quest to kill the King of Yam."], ["Main Questline: Prisoners of Yam."])
+
+
+speak = input("Do you want to approach? y/n")
+if "y":
+    print("I am humble potato. I seem to have a request for you young lad! My radish seems to have been gone for some time, can you help me find her?")
+else:
+    print("Goodbye kind lad! Hope to see you again!")
