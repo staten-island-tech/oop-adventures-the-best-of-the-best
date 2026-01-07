9881b256e69a593e9e1091e861ba92e1c47e3c47
cadee-exe <cadeez@nycstudents.net>
2026-01-07 08:44:02 -0500

    Create mc.py

---
+++ mc.py
@@ -0,0 +1,17 @@
+class mc:
+
+    def __init__(self, name, inventory, stats, dialogue, quest):
+        self.name = name
+        self.inventory = inventory
+        self.stats = stats
+        self.dialogue = dialogue
+        self.quest = quest
+
+
+    def approach(self):
+        speak = input("Do you want to approach? y/n")
+        if "y":
+            
+
+
+     
