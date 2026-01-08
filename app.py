0fa372d6efd995b6e61b304bdccd721f6e8db709
kexinl124 <KExinl10@nycstudents.net>
2026-01-08 09:26:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,19 +15,30 @@ class villian:
         else:
             print("Unvalid choice! Please put a yes or no")
 
-    def fight():
-        fight = input("Do you want to fight the Waverly Potato?(yes/no)").lower()
-        for "Hp" in {self.stats}:
-            if fight.lower() == "yes":
-                "Hp"-= 5
-            elif fight.lower() == "no":
+    def fight(self):
+            fight = input("Do you want to fight the Waverly Potato?(yes/no)").lower()
+            while fight.lower() == "yes":
+                Hp -= 20
+                self.fight
+                break
+            if fight.lower() == "no":
                 print(f"Hp" in {self.stats})
+                self.fight
             else:
                 print("Unvalid choice! Please put a yes/no")
+    
+    def talk(self):
+        speak = input("Who do you want to interact with?").lower()
+        if speak.lower() == "Waverly Potato":
+            print("Let's fight! I am so much better than you")
+        elif speak.lower() == "Humble Potato":
+            print("Give me all your money")
+        else: 
+            print("Character unfound! Please type an valid name")
 
-King_of_Yam = villian("King of Yam", ["Prisoners of Yam"], ["Hp" == 500, "Hunger" == 100, "Atk" == 500], ["I'm am the all mighty King of Yam"])
+King_of_Yam = villian("King of Yam", ["Prisoners of Yam"], ["Hp" == 500, "Atk" == 250], ["I'm am the all mighty King of Yam"])
 print(King_of_Yam.__dict__)
 
 
 
-        
+         
