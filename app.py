1dc2192e2525eb19f316f3b77acca01445a9517b
alinac1888 <alinac44@nycstudents.net>
2026-01-08 06:26:13 -0800

    Update app.py

--- app.py
+++ app.py
@@ -10,18 +10,29 @@ class npc:
         inventory = []
 
     def activity(self):
-        print()
+        print("nahaha")
 
     def robbery(self):
         random.choices(self.inventory)
-        self.inventory.
         if random.choice == "radish":
             print("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+            self.inventory.pop("radish")
         elif random.choices == "groceries":
             print("THEIRD THEIF")
-    
-    def 
+            self.inventory.pop("groceries")
 
 Humble_Potato = npc("Humble Potato", ["No thank you, its ok, fan.", "nananaan im so humble", "nananaan walking my radish"], ["Hello young lad!"], ["radish", "groceries","money"])        
 
 # "Thank you, with such gratitude and grace, for accepting request young, kind lad."
+class anpc:
+    def _init_(self):
+        self.name = anpc
+
+    def infect(self):
+        print("big cough, help me get medicine psl")
+        self.talk
+        print("thank yoaus kind sire")
+        self.stats -= 5
+
+
+sick_potato = npc("sick Potato", ["No thank you, its ok, fan.", "nananaan im so humble", "nananaan walking my radish"], ["Hello young lad!"], ["radish", "groceries","money"])
