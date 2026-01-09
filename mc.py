2620887e1c121fb7800b341f9a89fe1717c19950
cadee-exe <cadeez@nycstudents.net>
2026-01-09 09:09:11 -0500

    Update mc.py

--- mc.py
+++ mc.py
@@ -1,10 +1,8 @@
 class mc:
-    def __init__(self, name, inventory, stats, dialogue, quest, live):
+    def __init__(self, name, inventory, stats, live):
         self.name = name
         self.inventory = inventory
         self.stats = stats
-        self.dialogue = dialogue
-        self.quest = quest
         self.live = True
 
     def fight(self):
@@ -17,17 +15,17 @@ class mc:
         else:
             print(f"Get stronger, {self.name}. Maybe another day.")
 
-    
-Wavely_Potato = mc("Wavely Potato", ["Sword, healing orb, radishes"], ["Hp = 100, Atk = 20"], ["I come here to kill the King of Yam."], ["Main quest: Go to the village."], ["Live = True"])
-        
+
+Wavely_Potato = mc("Wavely_Potato", ["sword, healing orb"], ["health = 100, Atk = 20"], ["True"])
 
 def talk(self):
-        while self.live == True:
-            speak = input("Do you want to approach? y/n").lower()
-        if speak == "y":
-            print("I am humble potato. I seem to have a request for you young lad! My radish seems to have been gone for some time, can you help me find her? Press_Enter to find radish.")
-            print(f"Thank you {self.name}! Here is 10 carrot coins")
-    
-        else:
-            print(f"Goodbye {self.name}! Hope to see you again!")
-    
+    speak = input("Do you want to approach? y/n").lower()
+    if speak == "y":
+        print("I am humble potato. I seem to have a request for you young lad! My radish seems to have been gone for some time, can you help me find her? Press_Enter to find radish.")
+    else:
+        print(f"Goodbye {self.name}! Hope to see you again!")
+
+
+
+       
+        
