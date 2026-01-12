c7ab950430c28397e3b0c33cb1c726cc56c552d8
cadee-exe <cadeez@nycstudents.net>
2026-01-12 09:26:16 -0500

    Update mc.py

--- mc.py
+++ mc.py
@@ -1,31 +1,59 @@
 class mc:
-    def __init__(self, name, inventory, stats, live):
+    def __init__(self, name, inventory, health):
         self.name = name
         self.inventory = inventory
-        self.stats = stats
-        self.live = True
+        self.health = health
+       
+    def heal(self):
+        print("Would you like to heal? y/n")
+        choose = input
+        if choose == "y":
+            Hp += 10
+        if choose == "n":
+            print("u sure...? ok..")
+        else:
+            print("That is not a choice u dum")
+
+    def talk(self):
+        speak = input
+        print("Do you want to approach? y/n")
+        if speak == "y":
+            print("I am humble potato. I seem to have a request for you young lad! My radish seems to have been gone for some time, can you help me find her? Press_Enter to find radish.")
+        if speak == "n":
+            print("Goodbye! Come back soon, young lad!")
+        else:
+            print("That is not a choice u dum")
 
     def fight(self):
-        while self.live == True:
-            print(f"{self.name}, would you like to fight? y/n")
+        print(f"{self.name}, would you like to fight? y/n")
+        choose = input
         if "y":
-            print(f"{self.name}'s  stats are at {self.stats}.") 
+            print(f"{self.name}'s health is at {self.health}.") 
             Hp -= 5
-            print(f"{self.name}'s stats are at {self.stats}.")
-        else:
-            print(f"Get stronger, {self.name}. Maybe another day.")
+            print(f"{self.name}'s health are at {self.health}.")
+            print("Do you wish to heal? y/n")
+            if choose == "y":
+                Hp += 10
+            if choose == "n":
+                print("alright.")
+                print(f"{self.name}'s health is at {self.health}")
+            else:
+                print("That is not a choice u dum")
 
-
-Wavely_Potato = mc("Wavely_Potato", ["sword, healing orb"], ["health = 100, Atk = 20"], ["True"])
-
-def talk(self):
-    speak = input("Do you want to approach? y/n").lower()
-    if speak == "y":
-        print("I am humble potato. I seem to have a request for you young lad! My radish seems to have been gone for some time, can you help me find her? Press_Enter to find radish.")
+    def find(self):
+        print("You found something rare! Should you..")
+        print("1) Pick it up")
+        print("2) Leave it there")
+    choose = input
+    if choose == "1":
+        print("Woahh you got a cool sword")
+    if choose == "2":
+        print("Shouldve taken it but ok")
     else:
-        print(f"Goodbye {self.name}! Hope to see you again!")
+        print("That is not a choice u dum")
 
-
+    
+def start()
 
 
         
