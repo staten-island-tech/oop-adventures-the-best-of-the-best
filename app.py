bc096e0ba9bf146ae72f0a5c2ceb891299595511
kexinl124 <KExinl10@nycstudents.net>
2026-01-12 09:26:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -38,16 +38,18 @@ class villian:
             print("Character unfound! Please type an valid name")
 
     def living(self,Hp):
-        if Hp <= 50:
+        if 20 <= Hp <= 50:
             print(f"He is at low health. This is his stats {self.stats}" )
-        elif Hp == 0:
+        elif Hp <= 0:
             self.live == False
             print("He is dead")
         else:
             print(f"He is ok. This is his stats{self.stats}")
 
-King_of_Yam = villian("King of Yam", ["Prisoners of Yam"], ["Hp" == 500, "Atk" == 250], ["I'm am the all mighty King of Yam"], True)
+King_of_Yam = villian("King of Yam", ["Prisoners of Yam"], ["Hp" = 500, "Atk" = 250], ["I'm am the all mighty King of Yam"], True)
 print(King_of_Yam.__dict__)
+
+
 
 
 
