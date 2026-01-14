5132abd18071e7080a6d37d2ae6c1bfb9761593e
alinac1888 <alinac44@nycstudents.net>
2026-01-14 06:18:32 -0800

    Update nana.py

--- nana.py
+++ nana.py
@@ -1,3 +1,13 @@
+inport random
 class merchant:
-    def __init__(self, stock, dialogue, money):
+    def __init__(self, name, stock, dialogue, money):
         self.name = name
+        self.stock = stock
+        stock = []
+        self.dialogue = dialogue
+        dialogue = []
+        self.money = money
+        money = 12000
+
+    def welcome(self):
+        ran
