e37c2862801405f4f81850ce6c81d4d659a1737f
alinac1888 <alinac44@nycstudents.net>
2026-01-14 23:00:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import random
-import mc
 class npc:
     def _init_(self, name, dialogue, quest, inventory):
         self.name = name
@@ -36,15 +35,3 @@ class npc:
         self.inventory.remove(item)
 
 Humble_Potato = npc("Humble Potato", ["No thank you, its ok, fan.", "nananaan im so humble", "nananaan walking my radish"], ["Hello young lad!"], ["radish", "groceries","money"])        
-
-class anpc:
-    def _init_(self):
-        self.name = anpc
-
-    def infect(self):
-        print("big cough, help me get medicine psl")
-        mc.talk
-        print("thank yoaus kind sire")
-        mc.health -= 5
-        
-sick_potato = npc("sick Potato", ["No thank you, its ok, fan."], ["i am humble potato. i see to have a request for you young lad! My radish seems to have been gone for some time, can you help me find her?"], ["radish", "groceries","money"])
